20e0dbd44793b4814a323ce63113aac767e303de
Ivan <ivan@MacBook-Ivan.local>
2023-10-29 21:26:50 +0200

    Перший коміт

---
+++ calendar/main.py
@@ -0,0 +1,10 @@
+import calendar
+
+print('welcome to super calendar\n')
+
+year = int(input('Please, enter year:'))
+month = int(input('Please, enter something month:'))
+
+print(calendar.month(year,month))
+
+print('Best wishes!')
